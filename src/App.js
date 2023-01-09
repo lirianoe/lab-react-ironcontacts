e95222cca0e44a200a08c474f9ecea46ee681d91
Esther Liriano <estherlirianor@gmail.com>
2023-01-08 22:40:04 -0500

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsData from "./contacts.json"
+import { useState } from 'react'
+import trophy from './tropy.png'
+
 
 function App() {
+  
+  let [contacts, setContacts] = useState(contactsData.slice(0, 5))
+  const [remainingContacts, setRemainingContacts] = useState(contactsData.slice(5))
+  const [sortedContacts, setSortedContacts] = useState(contactsData)
+
+  const randomContact = () => {
+    let remainingContactsIndex = Math.floor(Math.random() * remainingContacts.length )
+    let randomContact = remainingContacts[remainingContactsIndex]
+    const newArr = [...contacts, randomContact]
+    setContacts(newArr)
+    const copyRemainigContacts = [...randomContact]
+    copyRemainigContacts.splice(remainingContactsIndex, 1)
+    setRemainingContacts(copyRemainigContacts)
+  }
+
+  const sortedContactsName = () => {
+    let sortedContacts
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button
+      onClick={randomContact}
+          >
+        Add Random Contact
+        </button>
+        
+        <table>
+          <thead>
+            <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+          </thead>
+          <tbody>
+     {contacts.map(el => {
+
+    
+     
+      return(
+        
+            <tr>
+            <td><img className='image' src={el.pictureUrl}/></td>
+            <td>{el.name}</td>
+            <td>{el.popularity}</td>
+            <td>{ el.wonOscar ? <img className='trophy' src={trophy}/> : null}</td>
+            <td>{ el.wonEmmy ? <img className='trophy' src={trophy}/>  : null }</td>
+          </tr>
+
+                )
+                })}
+          </tbody>
+          
+      </table>
     </div>
   );
 }
